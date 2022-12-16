b6053263dd81700d3c4b99f77c35e57df3b8fee5
Andy Le <andytle95@gmail.com>
2022-12-16 14:52:10 -0800

    runningSum started

--- L75/1day/runningSum.js
+++ L75/1day/runningSum.js
@@ -1,6 +1,21 @@
+// have to add all running sums in the array
+// ex. [1, 3, 5, 0] = [1+3, 1+3+5, 1+3+5+0];
+//i think?
+
+
+
 let runningSum = (nums) => {
     let arr = [];
     for (let i = 0; i < nums.length; i ++) {
-
+        let z = 0;
+        let first = nums[z];
+        if (first === nums[i]) {
+            arr.push(first);
+            z ++;
+        } else {
+            arr.push(nums[i] + nums[z]);
+            z ++;
+        }
     }
+    return arr;
 };

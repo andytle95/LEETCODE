b135293ab09e31256c3d06f9b86aebbe1b408941
Andy Le <andytle95@gmail.com>
2023-02-05 00:41:43 -0800

    threeSum

---
+++ array/threeSumClosest.js
@@ -0,0 +1,14 @@
+let threeSumClosest = (nums, target) => {
+    let total = 0;
+
+    for (let i = 0; i < nums.length; i ++) {
+        let number = nums[i];
+        if (target === number) {
+            nums.slice()
+
+    }
+}
+    console.log(nums);
+}
+
+threeSumClosest([-1,2,1,-4], 1);

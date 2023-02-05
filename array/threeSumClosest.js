a9b3282d1765726c81d132bb1c3407fc472be5b5
Andy Le <andytle95@gmail.com>
2023-02-05 09:35:34 -0800

    threeSum not passing 15/58 test cases passed

--- array/threeSumClosest.js
+++ array/threeSumClosest.js
@@ -3,12 +3,15 @@ let threeSumClosest = (nums, target) => {
 
     for (let i = 0; i < nums.length; i ++) {
         let number = nums[i];
-        if (target === number) {
-            nums.slice()
-
+    if (target === number) {
+        nums.splice(i, i - 1)
+        nums.unshift(target);
+        total = nums[0] + nums[1] + nums[2];
     }
 }
-    console.log(nums);
+    return total;
 }
 
-threeSumClosest([-1,2,1,-4], 1);
+// console.log(threeSumClosest([-1,2,1,-4], 1));
+// console.log(threeSumClosest([0, 0, 0], 1));
+console.log(threeSumClosest([0, 1, 2], 0));

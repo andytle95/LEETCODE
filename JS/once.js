dfa2814e2532afbea37468285d7102f575c5aba3
Andy Le <andytle95@gmail.com>
2023-06-01 13:37:54 -0700

    once started

---
+++ JS/once.js
@@ -0,0 +1,12 @@
+// Given a function fn, return a new function that is identical to the original
+// function except that it ensures fn is called at most once.
+
+// The first time the returned function is called,
+//  it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+
+let once = function(fn) {
+    return function(...args) {
+
+    }
+}

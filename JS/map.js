e42a18a394f38751c55f99983179e3792d54909b
Andy Le <andytle95@gmail.com>
2023-05-31 23:56:06 -0700

    map started

---
+++ JS/map.js
@@ -0,0 +1,10 @@
+let map = (arr, fn) => {
+    let array = [];
+
+    for (let i = 0; i < arr.length; i ++) {
+        if (fn(arr[i], i)) {
+            array.push(arr[i]);
+        }
+    }
+    return array;
+}

3c921ba727572439adcd4c5a7e97f27829236792
Andy Le <andytle95@gmail.com>
2023-06-08 21:47:12 -0700

    need a break

---
+++ neetcode-io/isAnagram.js
@@ -0,0 +1,9 @@
+let isAnagram = (s, t) => {
+    //definin object that will have counts for each letter passed through
+    let object = {};
+    let count = 0;
+    //split the strings into letters inside of an array to iterate through.
+    let s1 = s.split(' ');
+    let t1 = t.split(' ');
+
+}

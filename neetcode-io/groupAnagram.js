04af0beeab36cfd88b8e986929cc9a25e72f83a2
Andy Le <andytle95@gmail.com>
2023-06-09 19:14:35 -0700

    need to do more research

--- neetcode-io/groupAnagram.js
+++ neetcode-io/groupAnagram.js
@@ -1,3 +1,19 @@
-let groupAnagrams = (strs) => {
+var groupAnagrams = function(strs) {
+    //array that we return
+    let arr = [];
 
-}
+    //for loop to iterate over string
+    for (let i = 0; i < strs.length; i ++) {
+        // the actual letters
+        let letters = strs[i];
+        // define sorted letters
+        let anagram = letters.split('').sort().join('')
+        if (anagram === anagram) {
+            true;
+        }
+        if (true) {
+            arr.push(letters);
+        }
+    }
+    return arr;
+};
